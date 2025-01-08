b4b9746239a061fc0db78930c0c5eb7345fa24a5
Acun03 <nasrunasrimuhd@gmail.com>
2025-01-08 17:31:26 +0800

    oo

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
 
-Route::resource('products', ProductController::class);
+Route::get('/',function(){return view('welcome');});
