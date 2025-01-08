0ef786190f5bffc71f9b47d8407197a424d1069b
Acun03 <nasrunasrimuhd@gmail.com>
2025-01-09 00:53:03 +0800

    transferring gymlife-master to public

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-
-Route::get('/',function(){return view('welcome');});
+Route::get('/', function () {
+    return redirect()->to('gymlife-master/index.html');
+});
 Route::resource('products',ProductController::class);
