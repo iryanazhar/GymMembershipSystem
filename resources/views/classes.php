3b3298ca1a5417a94cafa9b23ed1640294b0f873
iryanazhar <iryansyauqi@gmail.com>
2025-01-09 13:33:48 +0800

    aku ada ubah sikit

--- resources/views/classes.php
+++ resources/views/classes.php
@@ -41,23 +41,12 @@
         </div>
         <nav class="canvas-menu mobile-menu">
             <ul>
-                <li><a href="./index.html">Home</a></li>
-                <li><a href="./about-us.html">About Us</a></li>
-                <li><a href="./class-details.html">Classes</a></li>
-                <li><a href="./services.html">Services</a></li>
-                <li><a href="./team.html">Our Team</a></li>
-                <li><a href="#">Pages</a>
-                    <ul class="dropdown">
-                        <li><a href="./about-us.html">About us</a></li>
-                        <li><a href="./class-timetable.html">Classes timetable</a></li>
-                        <li><a href="./bmi-calculator.html">Bmi calculate</a></li>
-                        <li><a href="./team.html">Our team</a></li>
-                        <li><a href="./gallery.html">Gallery</a></li>
-                        <li><a href="./blog.html">Our blog</a></li>
-                        <li><a href="./404.html">404</a></li>
-                    </ul>
-                </li>
-                <li><a href="./contact.html">Contact</a></li>
+                <li><a href="./index.html">Log In</a></li>
+                <li><a href="./package">Package</a></li>
+                <li><a href="./class">Classes</a></li>
+                <li><a href="./members">Members</a></li>
+                <li><a href="./team.html">Trainers</a></li>
+                <li><a href="./contact.html">Products</a></li>
             </ul>
         </nav>
         <div id="mobile-menu-wrap"></div>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,35 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 Route::get('/', function () {
-    return redirect()->to('gymlife-master/index.html');
+    return view('mainpage');
 });
+Route::get('/package', function () {
+    return view('package');
+});
+Route::get('/classes', function () {
+    return view('classes');
+});
+Route::get('/members', function () {
+    return view('members');
+});
+Route::get('/trainers', function () {
+    return view('trainers');
+});
+Route::get('/products', function () {
+    return view('products');
+});
+
 Route::resource('products',ProductController::class);
 Route::get('/products/list', function () {
     return view('products.list');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
